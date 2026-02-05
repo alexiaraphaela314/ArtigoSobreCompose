4b0b93b6d36a8fd1574bb26ca83351ea76eca2a1
alexiaraphaela314 <alexiaraphaela314@gmail.com>
2026-02-05 18:05:32 -0300

    Tela de artigo sobre o compose, concluido.

--- app/src/main/java/com/example/artigosobreocompose/MainActivity.kt
+++ app/src/main/java/com/example/artigosobreocompose/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.artigosobreocompose
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artigosobreocompose.ui.theme.ArtigoSobreOComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +29,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ArtigoSobreOComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingImage(title = stringResource(R.string.titleTutorial), introduction = stringResource(R.string.textIntroduction), description = stringResource(R.string.textDescription) )
                 }
             }
         }
@@ -31,17 +40,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GreetingText(title: String, introduction: String, description: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = title,
+        fontSize = 24.sp,
         modifier = modifier
+            .padding(10.dp, 10.dp)
     )
+
+    Text(
+        text = introduction,
+        fontSize = 18.sp,
+        textAlign = TextAlign.Justify,
+        modifier = modifier
+            .padding(10.dp, 10.dp)
+    )
+
+    Text(
+        text = description,
+        fontSize = 18.sp,
+        textAlign = TextAlign.Justify,
+        modifier = modifier
+            .padding(10.dp, 16.dp)
+    )
+}
+
+
+@Composable
+fun GreetingImage(title: String, introduction: String, description: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.composebackground)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image (
+            painter = image,
+            contentDescription = null
+        )
+
+        GreetingText(
+            title = title,
+            introduction = introduction,
+            description = description,
+            modifier = Modifier
+        )
+    }
+
+
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtigoSobreOComposeTheme {
-        Greeting("Android")
+        GreetingImage(stringResource(R.string.titleTutorial), introduction = stringResource(R.string.textIntroduction),
+            stringResource(R.string.textDescription)
+        )
     }
 }
